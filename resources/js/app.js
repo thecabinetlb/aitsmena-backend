728930158aa63520eba5771399469b3fb185d58d
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-07-29 15:25:13 +0300

    added recaptcha

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 import './assets/css/index.css';
-import axios from 'axios'
+import axios from 'axios';
 
 import { createApp } from 'vue';
 import { createHead } from '@unhead/vue';
@@ -7,47 +7,49 @@ import { MotionPlugin } from '@vueuse/motion';
 import { VueRecaptchaPlugin } from 'vue-recaptcha/head';
 import router from './router/index';
 
-import Header from './components/layouts/Header.vue'
-import Footer from './components/layouts/Footer.vue'
+import Header from './components/layouts/Header.vue';
+import Footer from './components/layouts/Footer.vue';
 
-import HomeView from './views/HomeView.vue'
-import ContactView from './views/ContactView.vue'
-import AboutView from './views/AboutView.vue'
-import IndustriesView from './views/Industries/IndustriesView.vue'
-import CareersView from './views/CareersView.vue'
-import ResourcesView from './views/Resources/ResourcesView.vue'
-import StoriesPostDetailsView from './views/Industries/StoriesPostDetailsView.vue'
-import BlogView from './views/Resources/Blog/BlogView.vue'
-import BlogPostDetailsView from './views/Resources/Blog/BlogPostDetailsView.vue'
+import HomeView from './views/HomeView.vue';
+import ContactView from './views/ContactView.vue';
+import AboutView from './views/AboutView.vue';
+import IndustriesView from './views/Industries/IndustriesView.vue';
+import CareersView from './views/CareersView.vue';
+import ResourcesView from './views/Resources/ResourcesView.vue';
+import StoriesPostDetailsView from './views/Industries/StoriesPostDetailsView.vue';
+import BlogView from './views/Resources/Blog/BlogView.vue';
+import BlogPostDetailsView from './views/Resources/Blog/BlogPostDetailsView.vue';
 
-const app = createApp({});
-const head = createHead();
+const createMyApp = (siteKey) => {
+    const app = createApp({});
+    const head = createHead();
 
-app.component('header-layout', Header);
-app.component('footer-layout', Footer);
-app.component('home-view', HomeView);
-app.component('contact-view', ContactView);
-app.component('about-view', AboutView);
-app.component('industries-view', IndustriesView);
-app.component('careers-view', CareersView);
-app.component('resources-view', ResourcesView);
-app.component('stories-post-details-view', StoriesPostDetailsView);
-app.component('blog-view', BlogView);
-app.component('blog-post-details-view', BlogPostDetailsView);
+    app.component('header-layout', Header);
+    app.component('footer-layout', Footer);
+    app.component('home-view', HomeView);
+    app.component('contact-view', ContactView);
+    app.component('about-view', AboutView);
+    app.component('industries-view', IndustriesView);
+    app.component('careers-view', CareersView);
+    app.component('resources-view', ResourcesView);
+    app.component('stories-post-details-view', StoriesPostDetailsView);
+    app.component('blog-view', BlogView);
+    app.component('blog-post-details-view', BlogPostDetailsView);
 
-app.use(head);
-app.use(MotionPlugin);
-// app.use(VueRecaptchaPlugin, {
-//   v2SiteKey: '6LcX3AsqAAAAAKG0sYoZ3G3oQxqvLE8VeXHfnN_P',
-// });
-// Configure VueRecaptchaPlugin
-let siteKey = '';
+    app.use(head);
+    app.use(MotionPlugin);
+    app.use(VueRecaptchaPlugin, {
+        v2SiteKey: siteKey,
+    });
+    app.use(router);
+
+    app.mount('#app');
+};
+
+// Fetch the reCAPTCHA site key before initializing the app
 axios.get('/api/v1/recaptcha-config').then(response => {
-  siteKey = response.data.v2SiteKey;
-  app.use(VueRecaptchaPlugin, {
-    v2SiteKey: siteKey,
-  });
+    const siteKey = response.data.v2SiteKey;
+    createMyApp(siteKey);
+}).catch(error => {
+    console.error('Failed to load reCAPTCHA site key:', error);
 });
-app.use(router);
-
-app.mount('#app');
